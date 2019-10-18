f3fbb386f8474ecd3508deb229aa243d3f90ef9e
shinoburc <dandydot@gmail.com>
2019-10-18 17:19:12 +0900

    update dokuwiki backup script.

--- backup-dokuwiki.sh
+++ backup-dokuwiki.sh
@@ -4,7 +4,17 @@
 #
 
 BACKUP_DIRECTORY="$HOME/Dropbox/backup/dokuwiki/"`date +'%Y%m%d'`
-DOKUWIKI_DIRECTORY="/usr/share/nginx/html/dokuwiki"
+DOKUWIKI_DIRECTORY="$HOME/work/docker/php/dokuwiki"
+
+BACKUP_TARGET_DIRECTORIES=" \
+  $DOKUWIKI_DIRECTORY/data/pages \
+  $DOKUWIKI_DIRECTORY/data/meta \
+  $DOKUWIKI_DIRECTORY/data/media \
+  $DOKUWIKI_DIRECTORY/data/media_attic \
+  $DOKUWIKI_DIRECTORY/data/media_meta \
+  $DOKUWIKI_DIRECTORY/data/attic \
+  $DOKUWIKI_DIRECTORY/conf \
+  "
 
 if [ ! -d $DOKUWIKI_DIRECTORY ] ; then
     echo Backup directory is not exist : $DOKUWIKI_DIRECTORY
@@ -12,7 +22,5 @@ if [ ! -d $DOKUWIKI_DIRECTORY ] ; then
 fi
 
 mkdir -p $BACKUP_DIRECTORY
-
-tar jcfh $BACKUP_DIRECTORY/dokuwiki.tar.bz2 $DOKUWIKI_DIRECTORY
-
+tar jcfh $BACKUP_DIRECTORY/dokuwiki.tar.bz2 $BACKUP_TARGET_DIRECTORIES
 echo Done : $BACKUP_DIRECTORY
